552677be5589f5a8326f1020d7d24c78c54aaed4
Inderdeep Klotia <Inderdeep.klotia@hotmail.com>
2021-05-11 12:14:30 -0400

    Moved logic methods to a separate module for better separation of code. This module is solely responsible for methods related to the user playing as the Code Breaker.

--- Logic/game_logic_codebreak_user.rb
+++ Logic/game_logic_codebreak_user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'secret_code_generator'
-require_relative 'check_for_win'
+require_relative 'logic_methods'
 require_relative '../Text & Interface/game_text'
 require_relative '../Text & Interface/clue_circles'
 
@@ -11,7 +11,7 @@ module GameLogicCodeBreakUser
   include SecretCodeGenerator
   include GameText
   include ClueCircles
-  include CheckForWin
+  include LogicMethods
 
   def run_codebreak_user
     # Call the appropriate method to house the computer's secret code,
@@ -38,56 +38,5 @@ module GameLogicCodeBreakUser
       turn += 1
     end
 
-
-
-
-  end
-
-  # Method to see the number of guesses the user got correct:
-  def check_guess(secret_code, user_guess)
-    # Array to house all exact matches:
-    arr_exact = check_exact(secret_code, user_guess)
-    # Array to house all of the generic matches:
-    arr_general = check_generic(secret_code, user_guess)
-
-    # Subtract the total number of general occurrences by the number of exact occurrences
-    len = arr_exact.length
-    len.times { arr_general.delete_at(0) }
-
-    # Return the clue array combined.
-    arr_exact + arr_general
-  end
-
-  # This method returns the number of correct exact matches from the user's guess.
-  def check_exact(secret_code, user_guess)
-    # Array to house all exact matches:
-    arr_exact = []
-
-    # Check only for exact matches:
-    user_guess.each_index do |i|
-      arr_exact.push('Y') if secret_code[i] == user_guess[i]
-    end
-    arr_exact
-  end
-
-  # This method returns the number of correct generic matches from the user's guess.
-  def check_generic(secret_code, user_guess)
-    # Variable to house all generic matches:
-    arr_general = []
-
-    x = user_guess.uniq
-    x.each do |val|
-      next unless secret_code.include?(val)
-
-      # Count the number of occurrences in total:
-      occurrences = secret_code.count(val)
-
-      # Add to the generic array based on the number of occurrences.
-      occurrences.times do
-        arr_general.push('X')
-      end
-    end
-
-    arr_general
   end
 end
