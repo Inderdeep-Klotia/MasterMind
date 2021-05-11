fa026e9179a0824555f64895f545abaa1e69c21a
Inderdeep Klotia <Inderdeep.klotia@hotmail.com>
2021-05-11 17:30:44 -0400

    Created an AI that can break the code in an average of 5 turns.

--- Logic/game_logic_codebreak_ai.rb
+++ Logic/game_logic_codebreak_ai.rb
@@ -1,20 +1,68 @@
 # frozen_string_literal: true
 
 require_relative '../Text & Interface/game_text'
+require_relative 'logic_methods'
 
 # This module is responsible for housing the methods involved with breaking the code when the user is playing as the
 # code maker and the AI is responsible for breaking the code.
 module GameLogicCodebreakAI
 
   include GameText
+  include LogicMethods
 
   def run_codebreak_AI
     # Obtain user inputted secret code.
     secret_code = user_secret_code(0)
 
+    # Set of all possible combinations:
+    set = create_set
 
+    12.times do |n|
+      # Starting Text
+      ai_turn_text(n + 1)
+      # Ai current guess
+      guess = set.sample
 
+      # Obtain the clues for the guess:
+      clues = check_guess(secret_code, guess)
 
+      # Display end turn value:
+      ai_turn_end(guess, clues)
+
+      if check_win(secret_code, guess)
+        ai_won_game(secret_code, guess, n + 1)
+        break
+      end
+
+      # Delete all values that don't return the same clues as the user's guess. The user's guess will act as the secret
+      # code
+      delete_values(set, clues, guess)
+
+      # Give some time intervals between turns.
+      sleep(1)
+    end
+  end
+
+  # Creates an array that houses all possible combinations.
+  def create_set
+    set = ('1'..'6').to_a
+    set = set.repeated_permutation(4).to_a
+    # Convert the set values to integers:
+    set.map do |i|
+      i.map!(&:to_i)
+    end
+
+    # Returns the set.
+    set
+  end
+
+  # Delete values from set:
+  def delete_values(set, clues, guess)
+    set.delete_if do |val|
+      clue_arr2 = check_guess(guess, val)
+      clues != clue_arr2
+    end
+    set.delete(guess)
   end
 
 end
--- run_game.rb
+++ run_game.rb
@@ -18,16 +18,10 @@ class RunGame
     # Call the method from Instructions
     puts starting_game_instructions
 
-    # Ask if the user wishes to play against another player or the computer.
-    if single_player?
-      # Ask User: Play as the CodeBreaker or CodeMaker?
-      selection = break_or_make?
-      # Have the user or the AI break the code based on the user input.
-      selection == '2' ? run_codebreak_user : run_codebreak_AI
-
-    else
-      puts 'If not single player, then multiplayer. Need to finish section.'
-    end
+    # Ask User: Play as the CodeBreaker or CodeMaker?
+    selection = break_or_make?
+    # Have the user or the AI break the code based on the user input.
+    selection == '2' ? run_codebreak_user : run_codebreak_AI
 
     # Ask if the user wants to play again:
     play_again?
